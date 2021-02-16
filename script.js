d05c02692eacde8483a55790af61c653ba6963df
vinayak-gaikwad <gaikwadvinayak291@gmail.com>
2021-02-16 18:32:12 +0530

    adding script html and css

--- script.js
+++ script.js
@@ -1,73 +1,120 @@
-//getting elements for dom manupilation
-var canvas = document.getElementById("my-canvas");
-var context = canvas.getContext("2d");
+const colors = document.querySelectorAll(".colors");
+const lineCap = document.getElementById("lineCap");
+const clearButton = document.getElementById("clear");
+const canvas = document.getElementById("canvas");
+const erase = document.getElementById("erase");
+const colorPicker = document.getElementById("colorPicker");
+const brush = document.getElementById("brush");
 
-// to get the coordinates of mouse pointer
-function getCanvasCoordinates(event) {
-  var rectangle = canvas.getBoundingClientRect();
-  var scaleX = canvas.width / rectangle.width;
-  var scaleY = canvas.height / rectangle.height;
-  console.log(canvas.height, rectangle.height);
-  var x = (event.clientX - rectangle.left) * scaleX;
-  var y = (event.clientY - rectangle.top) * scaleY;
-  return { x: x, y: y };
+console.log(colorPicker);
+var imageData;
+var startX = 0,
+    startY = 0,
+    mouseX = 0,
+    mouseY = 0;
+var isDrawing = false;
+var hasLoaded = false;
+var canvasWidth = window.innerWidth - 250;
+var canvasHeight = window.innerHeight - 8;
+var ctx = null;
+var bounds = null;
+var colourValue = "#000000";
+
+const slider = document.getElementById("stroke");
+
+slider.oninput = () => {
+    console.log(slider.value);
+    ctx.lineWidth = slider.value;
+};
+
+function downloadCanvas(el) {
+    const imageURI = canvas.toDataURL("image/png");
+    el.href = imageURI;
 }
 
-// if this function is not their lines are not drawn properly
-var snapshot;
-function takeSnapshot() {
-  snapshot = context.getImageData(0, 0, canvas.width, canvas.height);
-}
-function restoreSnapshot() {
-  context.putImageData(snapshot, 0, 0);
+function drawFree() {
+    ctx.lineTo(mouseX, mouseY);
+    ctx.stroke();
+    ctx.beginPath();
+    ctx.moveTo(mouseX, mouseY);
 }
 
-var dragging = false,
-  dragStartLocation;
-//drawing line
-function drawLine(position) {
-  context.beginPath();
-  context.moveTo(dragStartLocation.x, dragStartLocation.y);
-  context.lineTo(position.x, position.y);
-  context.stroke();
+function onmousedown(e) {
+    if (hasLoaded && e.button === 0) {
+        if (!isDrawing) {
+            startX = e.clientX - bounds.left;
+            startY = e.clientY - bounds.top;
+
+            isDrawing = true;
+        }
+
+        ctx.beginPath();
+        drawFree();
+    }
 }
 
-// when mouse cliked and dragged
-function dragDown(event) {
-  dragStartLocation = getCanvasCoordinates(event);
-  dragging = true;
-  takeSnapshot();
+function onmouseup(e) {
+    if (hasLoaded && e.button === 0) {
+        isDrawing = false;
+    }
+    drawFree();
+    ctx.beginPath()
 }
-// when mouse dragged
-function drag(event) {
-  var position;
-  if (dragging === true) {
-    restoreSnapshot();
-    position = getCanvasCoordinates(event);
-    drawLine(position);
-  }
+
+function onmousemove(e) {
+    if (hasLoaded) {
+        mouseX = e.clientX - bounds.left;
+        mouseY = e.clientY - bounds.top;
+
+        if (isDrawing) {
+            console.log(mouseX, mouseY);
+            drawFree();
+        }
+    }
 }
-// when mouse up and dragged
-function dragUp(event) {
-  dragging = false;
-  var position = getCanvasCoordinates(event);
-  drawLine(position);
-}
-// initiallization
+
 function init() {
-  let dpi = window.devicePixelRatio;
-  canvas.setAttribute("height", 700 * dpi);
-  canvas.setAttribute("width", 1400 * dpi);
-  context.strokeStyle = "blue";
-  context.lineWidth = 6;
-  context.lineCap = "square";
+    canvas.width = canvasWidth;
+    canvas.height = canvasHeight;
 
-  //when mouse is clicked
-  canvas.addEventListener("mousedown", dragDown, false);
-  //when mouse is moved
-  canvas.addEventListener("mousemove", drag, false);
-  //when mouse is uncliked
-  canvas.addEventListener("mouseup", dragUp, false);
+    bounds = canvas.getBoundingClientRect();
+
+    ctx = canvas.getContext("2d");
+    ctx.strokeStyle = "black";
+    ctx.lineCap = "round";
+    ctx.lineWidth = slider.value;
+
+    
+    canvas.addEventListener("mousedown", onmousedown, false);
+
+    canvas.addEventListener("mousemove", onmousemove, false);
+
+    canvas.addEventListener("mouseup", onmouseup, false);
+    ctx.clearRect(0, 0, canvasWidth, canvasHeight);
+    hasLoaded = true;
+
+    drawFree();
 }
-// when window is load
+
 window.addEventListener("load", init, false);
+
+colorPicker.onchange = (e) => {
+    colourValue = e.target.value;
+    ctx.strokeStyle = e.target.value;
+};
+
+clearButton.onclick = (e) => {
+    ctx.clearRect(0, 0, canvas.width, canvas.height);
+};
+
+brush.onclick = (e) => {
+    ctx.strokeStyle = colourValue;
+};
+
+erase.onclick = (e) => {
+    ctx.strokeStyle = "#ffffff";
+};
+
+lineCap.onchange = (e) => {
+    ctx.lineCap = e.target.value;
+};
